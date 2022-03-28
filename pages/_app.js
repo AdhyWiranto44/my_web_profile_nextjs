1e9e76c0e25bd4938a09cd864d3d255ab6671b18
adhywiranto44 <adhywiranto68@gmail.com>
2022-03-28 18:19:25 +0700

    Copy from old version of web profile

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../public/css/style.css'
+import '../public/js/script.js'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
