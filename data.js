9354c984e88376b3433ce3eeeaf0f4bdae9a4b4e
Adhy Wiranto Sudjana <adhy.sudjana@xtremax.com>
2023-05-04 14:29:51 +0700

    update profile 2023

--- data.js
+++ data.js
@@ -1,239 +1,251 @@
 const data = {
-  "web_icon": "icon-3.webp",
-  "nickname": "Adhy",
-  "full_name": "Adhy Wiranto",
-  "job": "Web Developer | Technology Enthusiast",
-  "social_media": [
+  web_icon: "icon-3.webp",
+  nickname: "Adhy",
+  full_name: "Adhy Wiranto",
+  job: "Frontend Developer | Technology Enthusiast",
+  social_media: [
     {
-      "link": "https://www.linkedin.com/in/adhy-wiranto-sudjana-s-t-665882155/",
-      "title": "LinkedIn",
-      "icon": "fab fa-linkedin",
+      link: "https://www.linkedin.com/in/adhy-wiranto-sudjana-s-t-665882155/",
+      title: "LinkedIn",
+      icon: "fab fa-linkedin",
     },
     {
-      "link": "https://github.com/adhywiranto44/",
-      "title": "Github",
-      "icon": "fab fa-github",
+      link: "https://github.com/adhywiranto44/",
+      title: "Github",
+      icon: "fab fa-github",
     },
     {
-      "link": "https://instagram.com/adhywiranto44/",
-      "title": "Instagram",
-      "icon": "fab fa-instagram",
+      link: "https://instagram.com/adhywiranto44/",
+      title: "Instagram",
+      icon: "fab fa-instagram",
     },
     {
-      "link": "https://twitter.com/AdhyWiranto44/",
-      "title": "Twitter",
-      "icon": "fab fa-twitter",
+      link: "https://twitter.com/AdhyWiranto44/",
+      title: "Twitter",
+      icon: "fab fa-twitter",
     },
     {
-      "link": "https://www.behance.net/skuukzkylxixsxa/",
-      "title": "Behance",
-      "icon": "fab fa-behance-square",
-    }
-  ],
-  "photo": "profile-4.webp",
-  "about_me": "Informatics Engineering graduate of Pasundan University, humble and skilled to build full-stack web applications and API using Node JS, React JS and Laravel. Good understanding of Object-Oriented Programming, Design Pattern, and Algorithm. Like to fix code, maintain code and explore new technologies. Experienced build projects in team, joined organization, and community. Looking for work opportunities and growth.",
-  "hobbies": [
-    {
-      "img": "reading-books.jpg",
-      "title": "Reading Books",
-      "heading": "Reading Books",
-      "description": "Find something new, implemented in real world. I recommended you to read Atomic Habits :)"
-    },
-    {
-      "img": "listening-to-music.jpg",
-      "title": "Listening to Music",
-      "heading": "Listening to Music",
-      "description": "POP and RnB."
-    },
-    {
-      "img": "youtube.jpg",
-      "title": "Youtube",
-      "heading": "Youtube",
-      "description": "Created programming videos, watch my subscripted channels"
+      link: "https://www.behance.net/skuukzkylxixsxa/",
+      title: "Behance",
+      icon: "fab fa-behance-square",
     },
   ],
-  "skills": [
+  photo: "profile-5.jpg",
+  about_me:
+    "Frontend Web Developer at PT. Xtremax Teknologi Indonesia, humble and skilled to build full-stack web applications and API using Node JS, React JS, Laravel, and Vue JS. Good understanding of Object-Oriented Programming, Design Pattern, and Algorithm. Like to fix code, maintain code and explore new technologies. Experienced build projects in team, joined organization, and community.",
+  hobbies: [
     {
-      "icon": "fas fa-laptop-code",
-      "heading": "Programming",
-      "description": "Web Development, RESTful API, Google Cloud"
+      img: "listening-to-music.jpg",
+      title: "Listening to Music",
+      heading: "Listening to Music",
+      description: "Catchy songs",
     },
     {
-      "icon": "fas fa-layer-group",
-      "heading": "UI/UX Design",
-      "description": "Figma, Adobe XD, Canva"
-    },
-    {
-      "icon": "fas fa-bezier-curve",
-      "heading": "Others",
-      "description": "Microsoft Office, Video Editing"
-    },
-  ],
-  "portfolio": [
-    {
-      "img": "moneytory.webp",
-      "heading": "Moneytory",
-      "year": "2022",
-      "description": "Moneytory adalah aplikasi berbasis website yang diperuntukan untuk oleh Usaha Mikro Kecil Menengah (UMKM) untuk mengelola inventory dan keuangan.",
-      "link": "https://moneytory.000webhostapp.com/",
-      "technology": [
-        {
-          "title": "Laravel",
-          "icon": "fab fa-php"
-        },
-        {
-          "title": "MongoDB",
-          "icon": "fas fa-database"
-        },
-        {
-          "title": "Bootstrap",
-          "icon": "fab fa-bootstrap"
-        },
-      ]
-    },
-    {
-      "img": "findmyhobby.webp",
-      "heading": "Find My Hobby",
-      "year": "2021",
-      "description": "Find My Hobby merupakan website yang berisi daftar hobi dan rekomendasi hobi yang banyak digemari orang-orang.",
-      "link": "http://findmyhobby.vercel.app/",
-      "technology": [
-        {
-          "title": "Node js",
-          "icon": "fab fa-node-js"
-        },
-        {
-          "title": "PostgreSQL",
-          "icon": "fas fa-database"
-        },
-        {
-          "title": "Bootstrap",
-          "icon": "fab fa-bootstrap"
-        },
-      ]
-    },
-    {
-      "img": "openblog.webp",
-      "heading": "Open Blog",
-      "year": "2021",
-      "description": "Open-Blog merupakan projek website blogging open-source untuk keperluan pribadi atau komersial.",
-      "link": "http://github.com/AdhyWiranto44/open-blog/",
-      "technology": [
-        {
-          "title": "Node js",
-          "icon": "fab fa-node-js"
-        },
-        {
-          "title": "MongoDB",
-          "icon": "fas fa-database"
-        },
-        {
-          "title": "Bootstrap",
-          "icon": "fab fa-bootstrap"
-        },
-      ]
-    },
-    {
-      "img": "twiceapi.webp",
-      "heading": "TWICE API",
-      "year": "2021",
-      "description": "API yang menyediakan informasi seputar TWICE seperti member dan album",
-      "link": "http://twice-api-server.herokuapp.com/",
-      "technology": [
-        {
-          "title": "Node js",
-          "icon": "fab fa-node-js"
-        },
-        {
-          "title": "Bootstrap",
-          "icon": "fab fa-bootstrap"
-        },
-      ]
-    },
-    {
-      "img": "ikasma3bdg.webp",
-      "heading": "IKASMA3BDG",
-      "year": "2020",
-      "description": "Web yang berisi data para alumni dengan fitur berita, forum bisnis, dan komunitas.",
-      "link": "https://ikasma3bdg.com",
-      "technology": [
-        {
-          "title": "CodeIgniter",
-          "icon": "fab fa-php"
-        },
-        {
-          "title": "Bootstrap",
-          "icon": "fab fa-bootstrap"
-        },
-      ]
-    },
-    {
-      "img": "sound.webp",
-      "heading": "SOUND",
-      "year": "2019",
-      "description": "Website E-Commerce CRUD yang menjual headphone dan earphone.",
-      "link": "https://github.com/AdhyWiranto44/sound",
-      "technology": [
-        {
-          "title": "CodeIgniter",
-          "icon": "fab fa-php"
-        },
-        {
-          "title": "Bootstrap",
-          "icon": "fab fa-bootstrap"
-        },
-      ]
+      img: "gaming-2.webp",
+      title: "Gaming",
+      heading: "Gaming",
+      description: "Casual games",
     },
   ],
-  "history": {
-    "educations": [
+  skills: [
+    {
+      icon: "fas fa-laptop-code",
+      heading: "Programming",
+      description: "Web Development, RESTful API, Google Cloud",
+    },
+    {
+      icon: "fas fa-layer-group",
+      heading: "UI/UX Design",
+      description: "Figma, Adobe XD, Canva",
+    },
+    {
+      icon: "fas fa-bezier-curve",
+      heading: "Others",
+      description: "Microsoft Office, Video Editing",
+    },
+  ],
+  portfolio: [
+    {
+      img: "moneytory.webp",
+      heading: "Moneytory",
+      year: "2022",
+      description:
+        "Moneytory adalah aplikasi berbasis website yang diperuntukan untuk oleh Usaha Mikro Kecil Menengah (UMKM) untuk mengelola inventory dan keuangan.",
+      link: "https://moneytory.000webhostapp.com/",
+      technology: [
+        {
+          title: "Laravel",
+          icon: "fab fa-php",
+        },
+        {
+          title: "MongoDB",
+          icon: "fas fa-database",
+        },
+        {
+          title: "Bootstrap",
+          icon: "fab fa-bootstrap",
+        },
+      ],
+    },
+    {
+      img: "findmyhobby.webp",
+      heading: "Find My Hobby",
+      year: "2021",
+      description:
+        "Find My Hobby merupakan website yang berisi daftar hobi dan rekomendasi hobi yang banyak digemari orang-orang.",
+      link: "http://findmyhobby.vercel.app/",
+      technology: [
+        {
+          title: "Node js",
+          icon: "fab fa-node-js",
+        },
+        {
+          title: "PostgreSQL",
+          icon: "fas fa-database",
+        },
+        {
+          title: "Bootstrap",
+          icon: "fab fa-bootstrap",
+        },
+      ],
+    },
+    {
+      img: "openblog.webp",
+      heading: "Open Blog",
+      year: "2021",
+      description:
+        "Open-Blog merupakan projek website blogging open-source untuk keperluan pribadi atau komersial.",
+      link: "http://github.com/AdhyWiranto44/open-blog/",
+      technology: [
+        {
+          title: "Node js",
+          icon: "fab fa-node-js",
+        },
+        {
+          title: "MongoDB",
+          icon: "fas fa-database",
+        },
+        {
+          title: "Bootstrap",
+          icon: "fab fa-bootstrap",
+        },
+      ],
+    },
+    {
+      img: "twiceapi.webp",
+      heading: "TWICE API",
+      year: "2021",
+      description:
+        "API yang menyediakan informasi seputar TWICE seperti member dan album",
+      link: "http://twice-api-server.herokuapp.com/",
+      technology: [
+        {
+          title: "Node js",
+          icon: "fab fa-node-js",
+        },
+        {
+          title: "Bootstrap",
+          icon: "fab fa-bootstrap",
+        },
+      ],
+    },
+    {
+      img: "ikasma3bdg.webp",
+      heading: "IKASMA3BDG",
+      year: "2020",
+      description:
+        "Web yang berisi data para alumni dengan fitur berita, forum bisnis, dan komunitas.",
+      link: "https://ikasma3bdg.com",
+      technology: [
+        {
+          title: "CodeIgniter",
+          icon: "fab fa-php",
+        },
+        {
+          title: "Bootstrap",
+          icon: "fab fa-bootstrap",
+        },
+      ],
+    },
+    {
+      img: "sound.webp",
+      heading: "SOUND",
+      year: "2019",
+      description:
+        "Website E-Commerce CRUD yang menjual headphone dan earphone.",
+      link: "https://github.com/AdhyWiranto44/sound",
+      technology: [
+        {
+          title: "CodeIgniter",
+          icon: "fab fa-php",
+        },
+        {
+          title: "Bootstrap",
+          icon: "fab fa-bootstrap",
+        },
+      ],
+    },
+  ],
+  history: {
+    educations: [
       {
-        "date": "August 2017 - August 2021",
-        "description": "Bachelor of Informatics Engineering, Pasundan University, Bandung",
-        "details": [
+        date: "August 2017 - August 2021",
+        description:
+          "Bachelor of Informatics Engineering, Pasundan University, Bandung",
+        details: [
           "GPA: 3,71 of 4,00",
           "Thesis: Created Indonesian Traffic Sign Dataset and implemented using Deep Learning Algorithm You Only Look Once (link: https://github.com/AdhyWiranto44/object-detection-indonesian-traffic-signs-using-yolo-algorithm/)",
-        ]
+        ],
       },
     ],
-    "experiences": [
+    experiences: [
       {
-        "date": "August 2021 - April 2022",
-        "description": "Github Repository Maintainer of Bellshade, Indonesia",
-        "details": [
+        date: "November 2022 - Today",
+        description: "Frontend Developer of Xtremax, Indonesia",
+        details: [
+          "Contributed on several internal projects",
+          "Learned how to contribute in professional field",
+          "Finished 30+ backlog ticket in 6 months on different projects",
+        ],
+      },
+      {
+        date: "August 2021 - April 2022",
+        description: "Github Repository Maintainer of Bellshade, Indonesia",
+        details: [
           "Maintained code repository in Github (link: https://bellshade.org/)",
           "Learned how to contribute on Open-Source Project",
           "Made free learning resource for Indonesian Learner",
-        ]
+        ],
       },
       {
-        "date": "June 2021 - April 2022",
-        "description": "Administrator or Web Programming UNPAS, Indonesia",
-        "details": [
+        date: "June 2021 - April 2022",
+        description: "Administrator or Web Programming UNPAS, Indonesia",
+        details: [
           "Helped other community member about programming",
           "Learned about discussion and teamwork",
           "Gathered more than 25,000 discord server members",
-        ]
+        ],
       },
       {
-        "date": "April - July 2020",
-        "description": "Web Developer of Ikatan Alumni SMA Negeri 3 Bandung",
-        "details": [
+        date: "April - July 2020",
+        description: "Web Developer of Ikatan Alumni SMA Negeri 3 Bandung",
+        details: [
           "Developed news portal of website IKASMA3BDG (link: https://www.ikasma3bdg.com/)",
           "Learned about teamwork",
-          "Achieved an A score in practical work courses at College",
-        ]
+          "Achieved an 'A' score in practical work courses at College",
+        ],
       },
       {
-        "date": "September 2019 - February 2021",
-        "description": "Laboratory Assistant of Informatics Engineering Department, Pasundan University, Bandung",
-        "details": [
+        date: "September 2019 - February 2021",
+        description:
+          "Laboratory Assistant of Informatics Engineering Department, Pasundan University, Bandung",
+        details: [
           "Teach topics such as Web Programming, Object-Oriented Programming, and Data Structure Concepts",
           "Coordinator Laboratory Assistant of Object-Oriented Programming and Mobile Application System Subjects",
-        ]
+        ],
       },
     ],
-  }
-}
+  },
+};
 
 module.exports = data;
--- pages/history.js
+++ pages/history.js
@@ -1,10 +1,9 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
-import data from '../data';
-import Navbar from '../components/Navbar';
-import HomeLayout from '../layouts/home';
-
+import data from "../data";
+import Navbar from "../components/Navbar";
+import HomeLayout from "../layouts/home";
 
 const History = () => {
   return (
@@ -18,22 +17,29 @@ const History = () => {
           </div>
           <div className="row" data-aos="fade-up" data-aos-offset="300">
             <div className="col">
-              <div className="card bg-0 border-0 shadow" style={{ borderRadius: "30px" }}>
+              <div
+                className="card bg-0 border-0 shadow"
+                style={{ borderRadius: "30px" }}
+              >
                 <div className="card-body">
                   <div className="row">
-                    <div className="col-md-6 my-3" data-aos="fade-right" data-aos-delay="500">
+                    <div
+                      className="col-md-6 my-3"
+                      data-aos="fade-right"
+                      data-aos-delay="500"
+                    >
                       <h5 className="text-center mb-3">Education</h5>
                       <ul className="timeline position-relative">
                         {data.history.educations.map((education, idx) => {
                           return (
                             <li key={idx} className="list-unstyled ps-3 mb-3">
                               <small>{education.date}</small>
-                              <p className="fw-bold mb-0">{education.description}</p>
+                              <p className="fw-bold mb-0">
+                                {education.description}
+                              </p>
                               <ul>
                                 {education.details.map((detail, idx) => {
-                                  return (
-                                    <li key={idx}>{detail}</li>
-                                  );
+                                  return <li key={idx}>{detail}</li>;
                                 })}
                               </ul>
                             </li>
@@ -41,19 +47,23 @@ const History = () => {
                         })}
                       </ul>
                     </div>
-                    <div className="col-md-6 my-3" data-aos="fade-left" data-aos-delay="500">
+                    <div
+                      className="col-md-6 my-3"
+                      data-aos="fade-left"
+                      data-aos-delay="500"
+                    >
                       <h5 className="text-center mb-3">Experience</h5>
                       <ul className="timeline position-relative">
                         {data.history.experiences.map((experience, idx) => {
                           return (
                             <li key={idx} className="list-unstyled ps-3 mb-3">
                               <small>{experience.date}</small>
-                              <p className="fw-bold mb-0">{experience.description}</p>
+                              <p className="fw-bold mb-0">
+                                {experience.description}
+                              </p>
                               <ul>
                                 {experience.details.map((detail, idx) => {
-                                  return (
-                                    <li key={idx}>{detail}</li>
-                                  );
+                                  return <li key={idx}>{detail}</li>;
                                 })}
                               </ul>
                             </li>
@@ -69,7 +79,7 @@ const History = () => {
         </div>
       }
     />
-  )
-}
+  );
+};
 
 export default History;
